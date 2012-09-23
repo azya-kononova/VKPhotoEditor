ac16d4b6c2e189f46c1802194f2976e613d89a30
Asya Kononova <asya.kononova@studiomobile.ru>
2012-09-23 15:23:06 +0400

    load images from photo library

--- VKPhotoEditor/StartViewController.m
+++ VKPhotoEditor/StartViewController.m
@@ -7,32 +7,92 @@
 //
 
 #import "StartViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import "ThumbnailsView.h"
 
-@interface StartViewController ()
-
+@interface StartViewController ()<ThumbnailsViewDataSource, ThumbnailsViewDelegate>
+- (IBAction)takePhoto:(id)sender;
+- (IBAction)cameraRoll:(id)sender;
 @end
 
-@implementation StartViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+@implementation StartViewController {
+    NSMutableArray *albumImages;
+    IBOutlet ThumbnailsView *gallery;
+    IBOutlet UIActivityIndicatorView *activityIndicator;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.navigationController.navigationBar.hidden = YES;
+    
+    [self loadAlbumImages];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)loadAlbumImages
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [activityIndicator startAnimating];
+    
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    albumImages = [NSMutableArray array];
+    
+    [library enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+         [group enumerateAssetsUsingBlock:^(ALAsset *asset, NSUInteger index, BOOL *stop) {
+             if (asset) {
+                 UIImage *image = [UIImage imageWithCGImage:[asset thumbnail]];
+                 [albumImages addObject:[[UIImageView alloc] initWithImage:image]];
+             }
+             
+             if (*stop) {
+                 albumImages = [NSMutableArray arrayWithArray:[[albumImages reverseObjectEnumerator] allObjects]];
+                 [gallery reloadData];
+                 [activityIndicator stopAnimating];
+             }
+             
+          }];
+     } failureBlock:^(NSError *error) {
+         NSLog(@"Can not get images from Photo Library.");
+     }];
+}
+
+
+#pragma mark - Actions
+
+- (IBAction)takePhoto:(id)sender
+{
+    
+}
+
+- (IBAction)cameraRoll:(id)sender
+{        
+    
+}
+
+
+#pragma mark - ThumbnailsViewDataSource
+
+- (NSUInteger)numberOfItemsInThumbnailsView:(ThumbnailsView*)view
+{
+    return albumImages.count;
+}
+
+- (UIView*)thumbnailsView:(ThumbnailsView*)view viewForItemWithIndex:(NSUInteger)index
+{
+    return [albumImages objectAtIndex:index];
+}
+
+- (CGFloat)thumbnailsView:(ThumbnailsView*)view thumbnailWidthForHeight:(CGFloat)height
+{
+    return height;
+}
+
+
+#pragma mark - ThumbnailsViewDelegate
+
+- (void)thumbnailsView:(ThumbnailsView *)view didTapOnItemWithIndex:(NSUInteger)index
+{
+    NSLog(@"Tap on image num.%d", index);
 }
 
 @end
