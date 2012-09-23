21e36d2c1852b8cee562fcdb2decca3b812bc2c1
Asya Kononova <asya.kononova@studiomobile.ru>
2012-09-23 20:06:12 +0400

    add filters selection on MakePhoto screen

--- VKPhotoEditor/CameraOverlayView.m
+++ VKPhotoEditor/CameraOverlayView.m
@@ -7,12 +7,59 @@
 //
 
 #import "CameraOverlayView.h"
+#import "ThumbnailsView.h"
+#import "Filters.h"
+
+@interface CameraOverlayView () <ThumbnailsViewDataSource, ThumbnailsViewDelegate>
+@end
 
 @implementation CameraOverlayView {
     IBOutlet UILabel *flashLabel;
+    IBOutlet ThumbnailsView *filtersView;
+    
+    IBOutlet UIButton *cancelBtn;
+    IBOutlet UIButton *photoBtn;
+    IBOutlet UIButton *filterBtn;
+    
+    NSArray *filters;
+    NSArray *movingButtons;
 }
 
 @synthesize picker;
+
+- (void)awakeFromNib
+{
+    filters = Filters.filters;
+    movingButtons = [NSArray arrayWithObjects:cancelBtn, photoBtn, filterBtn, nil];
+}
+
+
+#pragma mark ThumbnailView datasourse
+
+- (NSUInteger)numberOfItemsInThumbnailsView:(ThumbnailsView*)view
+{
+    return filters.count;
+}
+- (UIView*)thumbnailsView:(ThumbnailsView*)view viewForItemWithIndex:(NSUInteger)index
+{
+    return [filters objectAtIndex:index];
+}
+- (CGFloat)thumbnailsView:(ThumbnailsView*)view thumbnailWidthForHeight:(CGFloat)height
+{
+    return height;
+}
+
+
+#pragma mark ThumbnailView delegate
+
+- (void)thumbnailsView:(ThumbnailsView*)view didScrollToItemWithIndex:(NSUInteger)index
+{
+    
+}
+- (void)thumbnailsView:(ThumbnailsView *)view didTapOnItemWithIndex:(NSUInteger)index
+{
+    
+}
 
 
 #pragma mark - Actions
@@ -29,7 +76,18 @@
 
 - (IBAction)filters:(id)sender
 {
+    filtersView.hidden = !filtersView.hidden;
     
+    [filtersView reloadData];
+    NSInteger direction = filtersView.hidden ? -1 : 1;
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        for (UIButton *btn in movingButtons) {
+            CGRect frame = btn.frame;
+            frame.origin.y += direction * 15;
+            btn.frame = frame;
+        }
+    }];
 }
 
 - (IBAction)flash:(id)sender
