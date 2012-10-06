717ae20acf0210a29f7565f3abd4886499fb0684
Asya Kononova <asya.kononova@studiomobile.ru>
2012-10-07 00:19:32 +0400

    add filters

--- VKPhotoEditor/Filters.m
+++ VKPhotoEditor/Filters.m
@@ -8,13 +8,18 @@
 
 #import "Filters.h"
 #import "ImageFilter.h"
-#import "GPUImageSepiaFilter.h"
-#import "GPUImageGrayscaleFilter.h"
-#import "GPUImageEmptyFilter.h"
+#import "GPUImage.h"
 
-NSString *SepiaFilterName = @"SepiaFilter";
+NSString *MonochromeFilterName = @"MonochromeFilter";
 NSString *DefaultFilterName = @"DefaulFilter";
 NSString *GrayscaleFilterName = @"GrayscaleFilter";
+NSString *VignetteFilterName = @"VignetteFilter";
+NSString *KuwaharaFilterName = @"KuwaharaFilter";
+NSString *HazeFilterName = @"HazeFilter";
+NSString *SharpenFilterName = @"SharpenFilter";
+NSString *SaturationFilterName = @"SaturationFilter";
+NSString *ContrastFilterName = @"ContrastFilter";
+NSString *ToonFilterName = @"ToonFilter";
 
 @implementation Filters
 
@@ -22,16 +27,51 @@ NSString *GrayscaleFilterName = @"GrayscaleFilter";
 {
     
     return [NSArray arrayWithObjects: [[ImageFilter alloc] initWithPreviewPath:@"Basic.png" name:DefaultFilterName],
-            [[ImageFilter alloc] initWithPreviewPath:@"Filter1.png" name:SepiaFilterName],
-            [[ImageFilter alloc] initWithPreviewPath:@"Filter2.png" name:GrayscaleFilterName], nil];
+            [[ImageFilter alloc] initWithPreviewPath:@"Filter1.png" name:MonochromeFilterName],
+            [[ImageFilter alloc] initWithPreviewPath:@"Filter2.png" name:GrayscaleFilterName],
+            [[ImageFilter alloc] initWithPreviewPath:@"Filter3.png" name:VignetteFilterName],
+            [[ImageFilter alloc] initWithPreviewPath:@"Filter4.png" name:HazeFilterName],
+            [[ImageFilter alloc] initWithPreviewPath:@"Filter5.png" name:SharpenFilterName],
+            [[ImageFilter alloc] initWithPreviewPath:@"Filter6.png" name:SaturationFilterName],
+            [[ImageFilter alloc] initWithPreviewPath:@"Filter7.png" name:ContrastFilterName],
+            [[ImageFilter alloc] initWithPreviewPath:@"Filter8.png" name:ToonFilterName], nil];
 }
 
 + (GPUImageFilter*)GPUFilterWithName:(NSString*)name
 {
-    if (name == SepiaFilterName)
-        return [GPUImageSepiaFilter new];
+    if (name == MonochromeFilterName)
+        return [GPUImageMonochromeFilter new];
     if (name == GrayscaleFilterName)
         return [GPUImageGrayscaleFilter new];
+    if (name == VignetteFilterName) {
+        GPUImageFilter *filter = [GPUImageVignetteFilter new];
+        [(GPUImageVignetteFilter *)filter setVignetteStart:0.1];
+        return filter;
+    }
+    if (name == KuwaharaFilterName)
+        return [GPUImageKuwaharaFilter new];
+    if (name == HazeFilterName)
+        return [GPUImageHazeFilter new];
+    if (name == SharpenFilterName) {
+        GPUImageFilter *filter = [GPUImageSharpenFilter new];
+        [(GPUImageSharpenFilter *)filter setSharpness:2.0];
+        return filter;
+    }
+    if (name == SaturationFilterName) {
+        GPUImageFilter *filter = [GPUImageSaturationFilter new];
+        [(GPUImageSaturationFilter*)filter setSaturation:1.8];
+        return filter;
+    }
+    if (name == ContrastFilterName) {
+        GPUImageFilter *filter = [GPUImageContrastFilter new];
+        [(GPUImageContrastFilter *)filter setContrast:2.7];
+        return filter;
+    }
+    if (name == ToonFilterName) {
+        GPUImageFilter *filter = [GPUImageToonFilter new];
+        [(GPUImageToonFilter *)filter setQuantizationLevels:5];
+        return filter;
+    }
     else
         return [GPUImageEmptyFilter new];
 }
