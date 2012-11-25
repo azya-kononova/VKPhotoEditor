b4fa793f6213a023d5329a65ff15d742dcb5ce2c
asya <asya.kononova@gmail.com>
2012-11-25 13:44:31 +0400

    add fast viewer (just classes)

---
+++ VKPhotoEditor/FastViewerController.h
@@ -0,0 +1,13 @@
+//
+//  FastViewerController.h
+//  VKPhotoEditor
+//
+//  Created by asya on 11/25/12.
+//  Copyright (c) 2012 GirlsWhoDeveloping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FastViewerController : UIViewController
+
+@end
---
+++ VKPhotoEditor/FastViewerController.m
@@ -0,0 +1,38 @@
+//
+//  FastViewerController.m
+//  VKPhotoEditor
+//
+//  Created by asya on 11/25/12.
+//  Copyright (c) 2012 GirlsWhoDeveloping. All rights reserved.
+//
+
+#import "FastViewerController.h"
+
+@interface FastViewerController ()
+
+@end
+
+@implementation FastViewerController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
